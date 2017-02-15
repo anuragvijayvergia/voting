d637a8a116808ba7298af8080c9f866779b85282
Anurag Vijayvergia <anurag@readwhatiread.com>
2017-02-15 20:46:57 +0530

    11

--- chaincode_finished.go
+++ chaincode_finished.go
@@ -88,10 +88,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
-	// Handle different functions
-	// if function == "getVoteCount" {
-	// 	return t.getVoteCount(stub, args)
-	// } else if function == "getVotes" {
+//	Handle different functions
+	if function == "read" {
+		return t.read(stub, args)
+	 } //else if function == "getVotes" {
 	// 	return t.getVotes(stub, args)
 	// }
 	fmt.Println("query did not find func: " + function)
@@ -160,6 +160,24 @@ func (t *SimpleChaincode) createPoll(stub shim.ChaincodeStubInterface, args []st
 	return nil, nil
 }
 
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
 func (t *SimpleChaincode) vote(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
 	if len(args) != 2 {
@@ -206,3 +224,4 @@ func (t *SimpleChaincode) vote(stub shim.ChaincodeStubInterface, args []string) 
 
 	return nil, nil
 }
+
